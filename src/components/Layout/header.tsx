1f7343c8a00df81434841568d56cb606e5e5c4f6
Samsung <bitup.games@gmail.com>
2022-08-15 20:28:47 +0200

    fix: refactor navlink elements

--- src/components/Layout/header.tsx
+++ src/components/Layout/header.tsx
@@ -1,7 +1,23 @@
-import { Avatar, DarkThemeToggle, Dropdown, Navbar } from "flowbite-react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { DarkThemeToggle, Navbar } from "flowbite-react";
+import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 import { textContent } from "../../constants";
-import Image from "next/image";
+
+type LinkProps = {
+  label: string;
+  href: string;
+};
+const NavLink = ({ label, href }: LinkProps) => {
+  return (
+    <Link href={href}>
+      <a className="ml-1 py-3 font-semibold text-gray-500 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-500 sm:py-0">
+        {label}
+      </a>
+    </Link>
+  );
+};
 
 const Header: FC<Record<string, never>> = function () {
   return (
@@ -20,39 +36,13 @@ const Header: FC<Record<string, never>> = function () {
           </span>
         </Navbar.Brand>
         <div className="flex md:order-2">
-          <Dropdown
-            inline
-            label={
-              <Avatar
-                alt="User settings"
-                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                rounded
-              />
-            }
-          >
-            <Dropdown.Header>
-              <span className="block text-sm">Bonnie Green</span>
-              <span className="block truncate text-sm font-medium">
-                name@flowbite.com
-              </span>
-            </Dropdown.Header>
-            <Dropdown.Item>Dashboard</Dropdown.Item>
-            <Dropdown.Item>Settings</Dropdown.Item>
-            <Dropdown.Item>Earnings</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item>Sign out</Dropdown.Item>
-          </Dropdown>
           <Navbar.Toggle />
           <DarkThemeToggle />
         </div>
         <Navbar.Collapse>
-          <Navbar.Link href="/" active>
-            Home
-          </Navbar.Link>
-          <Navbar.Link href="/">About</Navbar.Link>
-          <Navbar.Link href="/">Services</Navbar.Link>
-          <Navbar.Link href="/">Pricing</Navbar.Link>
-          <Navbar.Link href="/">Contact</Navbar.Link>
+          {textContent.navbar.links.map((item, i) => (
+            <NavLink key={i} label={item.label} href={item.href} />
+          ))}
         </Navbar.Collapse>
       </Navbar>
     </header>
