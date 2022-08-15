01740b981832870afcb9c30206016ead1facfd9a
Samsung <bitup.games@gmail.com>
2022-08-15 11:29:57 +0200

    feat: add helpers

---
+++ src/lib/helpers/get-rgba-string.ts
@@ -0,0 +1,7 @@
+export function getRgbaString(currentPalette: number[]): string {
+  let numbersColorString = currentPalette.map((number) => number.toString());
+  let numbersColorParsed = numbersColorString.reduce((a, b) =>
+    a.concat(`, ${b}`)
+  );
+  return `rgba(${numbersColorParsed})`;
+}
---
+++ src/lib/helpers/similar-color.ts
@@ -0,0 +1,24 @@
+import { paletteList } from "./../../constants";
+type Color = number[];
+function colorSim(rgbColor: Color, compareColor: Color) {
+  let d = 0;
+  for (let i = 0; i < rgbColor.length; i++) {
+    d += (rgbColor[i]! - compareColor[i]!) * (rgbColor[i]! - compareColor[i]!);
+  }
+  return Math.sqrt(d);
+}
+
+export default function similarColor(
+  actualColor: Color,
+  currentPalette: number
+) {
+  let selectedColor: Color = [0, 0, 0];
+  let currentSim = colorSim(actualColor, paletteList[currentPalette]![0]!);
+  paletteList[currentPalette]?.forEach((color) => {
+    if (colorSim(actualColor, color) <= currentSim) {
+      selectedColor = color;
+      currentSim = colorSim(actualColor, color);
+    }
+  });
+  return selectedColor;
+}
