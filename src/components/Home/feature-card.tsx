2dc75a5d760afc4725e7f3c168df23dc1cade6c6
Samsung <bitup.games@gmail.com>
2022-08-16 17:19:03 +0200

    fix: update text styles

--- src/components/Home/feature-card.tsx
+++ src/components/Home/feature-card.tsx
@@ -6,10 +6,12 @@ interface Props {
 }
 export default function FeatureCard({ title, items }: Props) {
   return (
-    <div className="w-80">
+    <div className="w-80 sm:w-96">
       <Card>
-        <h1 className="text-lg font-bold dark:text-white">{title}</h1>
-        <div className="h-60 px-2 sm:h-72">
+        <h1 className="ml-2 mt-2 text-lg font-bold dark:text-white sm:text-xl">
+          {title}
+        </h1>
+        <div className="h-64 px-2 sm:h-72">
           <ul className="list-disc text-slate-800 dark:text-slate-300">
             {items.map((item, i) => (
               <li key={i} className="mt-1">
